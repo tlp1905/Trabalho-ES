93598fed9ae9aaf27628458073d76fdb7776aa0e
tlp1905 <145682351+tlp1905@users.noreply.github.com>
2023-09-23 13:00:34 +0100

    ola
    33

---
+++ TrabalhoES2023/src/oal/OneOrTwo.java
@@ -0,0 +1,12 @@
+package oal;
+
+public class OneOrTwo {
+static int ola(int a ) {
+	if(a <= 1 ) {
+		return 1;
+		
+	} 
+	
+	return 2;
+}
+}
---
+++ TrabalhoES2023/src/oal2/oo.java
@@ -0,0 +1,11 @@
+package oal2;
+
+public class oo {
+public int ooo(int b) {
+	if(b < 1) {
+		return 2;
+		
+	}
+	return 3;
+}
+}
